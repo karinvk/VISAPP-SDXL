03b159f57bb7a1bb18cbf0ece0c8812dbb429072
karinvk <75458512+karinvk@users.noreply.github.com>
2024-03-10 16:55:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main(args):
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9) #optimizer = torch.optim.Adam(params, lr=0.0001)
 
     # Train and save model
-    train_losses,valid_accuracies=train(train_loader, test_loader, model, optimizer, criterion, args.epochs, device, target_accuracy=None, model_save_path='./saved')
+    train_losses, valid_accuracies, avg_ap_score, avg_precision_score, avg_recall_score=train(train_loader, test_loader, model, optimizer, criterion, args.epochs, device, target_accuracy=None, model_save_path='./saved')
     df = pd.DataFrame({'train_loss': train_losses, 'valid_accuracy': valid_accuracies})
     file_name = f"results_{args.pos_percentage}.csv"
     df.to_csv(file_name, index=False)
@@ -43,6 +43,8 @@ if __name__ == "__main__":
     parser.add_argument('--dataset_path', type=str, default='.', help='Path to the KolektorSDD2 dataset')
     parser.add_argument('--epochs',type=int,default=10,help='epochs, default 10')
     parser.add_argument('--lr',type=float, default=0.001,help='learning rate, default 0.001')
-    parser.add_argument('--pos_percentage', type=float, default=1, help='keep certain percentage from all defect images when training, 0-1')
+    parser.add_argument('--num_pos_original', type=int, default=246, help='keep certain number of defect images coming from the original dataset when training, default is keeping all 246 positive images')
+    parser.add_argument('--num_pos_generated', type=int, default=0, help='add certain number of synthesized defect images, default is 0')
+    parser.add_argument('--root_pos_generated', type=str, default=None, help='synthesized defect images folder')
     args = parser.parse_args()
     main(args)
