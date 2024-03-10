ab4e1e7da872c300759cd4ca5051e9eab0080fa3
karinvk <75458512+karinvk@users.noreply.github.com>
2024-03-10 21:39:09 +0100

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def train_vis_by_epoch(train_losses,valid_accuracies,test_name):
+def train_vis_by_epoch(train_losses,valid_accuracies,experiment_name):
     df = pd.DataFrame({'train_loss': train_losses, 'valid_accuracy': valid_accuracies})
-    #file_name = f"epoch_{args.evaluation_model}_{args.num_pos_original}_{args.root_pos_generated}_{args.num_pos_generated}.csv"
-    #df.to_csv(file_name, index=False)
+    file_name = f"epoch_{experiment_name}.csv"
+    df.to_csv(file_name, index=False)
+    
     plt.plot(train_losses, label=f'Train Loss {test_name}')
     plt.plot(valid_accuracies, label=f'Val Accuracy {test_name}') 
     plt.legend(loc='upper left', bbox_to_anchor=(1, 1))  
@@ -12,7 +13,7 @@ def train_vis_by_epoch(train_losses,valid_accuracies,test_name):
     plt.ylabel('Loss / Accuracy')
     plt.title('Training Loss and Validation Accuracy')
     plt.grid(True)
-    plt.savefig(f'epoch_{test_name}.png')
+    plt.savefig(f'epoch_{experiment_name}.png')
     plt.close()
 
 def ap_result_visual(csv_result_root_name):
