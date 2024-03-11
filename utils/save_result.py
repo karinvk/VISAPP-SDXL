46a69665af104278e3dd72486cf80ada0804de80
karinvk <75458512+karinvk@users.noreply.github.com>
2024-03-11 10:48:33 +0100

    Update and rename save_ap_result.py to save_result.py

--- utils/save_result.py
+++ utils/save_result.py
@@ -1,6 +1,21 @@
 import pandas as pd
 import os
 
+def train_vis_by_epoch(train_losses,valid_accuracies,experiment_name):
+    df_epoch = pd.DataFrame({'train_loss': train_losses, 'valid_accuracy': valid_accuracies})
+    file_name = f"epoch_{experiment_name}.csv"
+    df_epoch.to_csv(file_name, index=False)
+    
+    plt.plot(train_losses, label=f'Train Loss {test_name}')
+    plt.plot(valid_accuracies, label=f'Val Accuracy {test_name}') 
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))  
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss / Accuracy')
+    plt.title('Training Loss and Validation Accuracy')
+    plt.grid(True)
+    plt.savefig(f'epoch_{experiment_name}.png')
+    plt.close()
+    
 def ap_result(valid_accuracies,avg_ap_score,avg_precision_score,avg_recall_score,csv_result_root_name):
     max_valid_acc=max(valid_accuracies)
     
